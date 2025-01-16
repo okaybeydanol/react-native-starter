263d009d4aac3dc2ed4196785f0e3cd2f356ba62
okaybeydanol <okay.beydanol@gmail.com>
2025-01-16 21:29:06 +0300

    Add alias configuration for absolute paths in TypeScript and Babel.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@helpers': './src/helpers',
+          '@hooks': './src/hooks',
+          '@routes': './src/routes',
+          '@store': './src/store',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
