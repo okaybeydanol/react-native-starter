89c4d770a81e3aa01d3567705ffe68cfc803f4be
okaybeydanol <okay.beydanol@gmail.com>
2025-04-02 06:50:26 +0300

    fix: specify type for useQuery in useUsers hook

--- src/query/queries/useUsers.ts
+++ src/query/queries/useUsers.ts
@@ -4,9 +4,10 @@ import {useQuery} from '@tanstack/react-query';
 // Internal Imports (Absolute)
 import {getAllUsers} from '@query/api/apis';
 import {queryKeys} from '@query/api/keys';
+import type {UsersResponse} from '@query/api/types';
 
 export const useUsers = () => {
-  return useQuery({
+  return useQuery<UsersResponse>({
     queryKey: queryKeys.users(),
     queryFn: getAllUsers,
   });
