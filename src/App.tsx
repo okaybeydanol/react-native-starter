c26360757d8ee0c18da1d1023c89fd1426856d77
okaybeydanol <okay.beydanol@gmail.com>
2025-04-14 00:51:33 +0300

    Update package.json to fix cache cleaner script and adjust App component for status bar height

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,15 @@ import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 
 // Internal Imports (Absolute)
+import {STATUS_BAR_HEIGHT} from '@constants/dimensions';
 import AppNavigator from '@navigation/AppNavigator';
 import {persistor, store} from '@store/store';
 
 const App = (): React.JSX.Element => {
   return (
-    <GestureHandlerRootView style={styles.container} testID="app-root">
+    <GestureHandlerRootView
+      style={[styles.container, {paddingTop: STATUS_BAR_HEIGHT}]}
+      testID="app-root">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <AppNavigator />
